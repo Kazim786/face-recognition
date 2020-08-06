6dfd0002a8cd7e0652f20e32a1ba8d7f5b5a9f94
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-06 17:48:56 -0500

    styling the site

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
     return (
         <div className= 'ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img src="https://img.icons8.com/emoji/48/000000/brain-emoji.png" style = {{height: '100px', paddingTop: '11px'}}/> </div>
+                <div className="Tilt-inner pa3"> <img  src="https://img.icons8.com/emoji/48/000000/brain-emoji.png"  style = {{height: '100px', paddingTop: '11px'}}  /> </div>
             </Tilt>
             
         </div>
