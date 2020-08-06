8ecd61a4e0eb77b7aaf7021922409e70d6de4c60
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-06 18:15:21 -0500

    changed cursor for buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import Navigation from './components/navigation/navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+    {  /*<Logo />
+      <ImageLinkForm />
+    <FaceRecognition />*/}
     </div>
   );
 }
