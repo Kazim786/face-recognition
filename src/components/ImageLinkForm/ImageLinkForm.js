168c114e47397e6ccb6927fcbbd10d6bc3b14ce2
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-06 17:55:20 -0500

    added shadow effect

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'tachyons'
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className = 'f3'>
+                {'This Magic Brain will detect faces in your Pictures'}
+                <div className= 'p3'>
+                    <img src="https://img.icons8.com/emoji/48/000000/eyes-emoji.png" alt= 'brain'/>  
+                </div>
+                <div className = 'pa4 br3 shadow-5'>
+                    <div className = 'p3'>
+                        <input className= 'f4 pa2 w-70 center' type= 'text' placeholder= 'Enter URL for the images'></input>
+                        <div className = 'p3'>
+                            <button className= 'w-30 grow f4 link ph3 pv2 dib white bg-light-green'>Detect!</button>
+                        </div>
+                    </div>
+                </div>
+            </p>
+        </div>
+    )
+}
+// pa4 br3 shadow-5
+export default ImageLinkForm
