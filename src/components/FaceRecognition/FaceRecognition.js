33882393f70aa6c88400a81c05846cc35922455f
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-07 14:01:55 -0500

    API Error

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl }) => {
     return (
         <div className = 'cemter ma'>
             <div className= 'absolute mt2'>
-                <img width='500px' height='auto' src = {imageUrl} alt = 'sample'></img>
+                <img width='500px' height='auto' src = {imageUrl} className = 'center' alt = 'sample'></img>
             </div>
             
         </div>
