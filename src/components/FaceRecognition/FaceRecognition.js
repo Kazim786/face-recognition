4cbea6c8c8ad53bc0ae3cd1dab450908491b7e25
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-08-07 13:59:51 -0500

    fixed image sizing

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,8 +3,11 @@ import React from 'react'
 
 const FaceRecognition = ({ imageUrl }) => {
     return (
-        <div className = 'cemter'>
-            <img src = {imageUrl} alt = 'sample'></img>
+        <div className = 'cemter ma'>
+            <div className= 'absolute mt2'>
+                <img width='500px' height='auto' src = {imageUrl} alt = 'sample'></img>
+            </div>
+            
         </div>
 
     )
